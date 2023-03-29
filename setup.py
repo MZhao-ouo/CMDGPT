1173fb24e31de836cc0785f41c72e5be764cc451
MZhao-WSL <mzhao.ouo@gmail.com>
2023-03-29 14:59:08 +0800

    添加pypi长描述

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from pathlib import Path
 with open(Path(__file__).absolute().parents[0] / "cmdgpt" / "VERSION") as _f:
     __version__ = _f.read().strip()
     
-with open("README.md", "r", encoding="utf-8") as f:
+with open("README-en.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 install_requires = ["requests", "colorama"]
@@ -13,7 +13,9 @@ setup(
     name="cmdgpt",
     version=__version__,
     packages=find_packages(),
-    description="A short description of your package",
+    description="Interact with the command line using natural language. | 用自然语言操控命令行",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=install_requires,
     license="MIT",
     url="https://github.com/MZhao-ouo/CMDGPT",
